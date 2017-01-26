41fde124a6897452dc9f31417d5e3225af6c6e89
Jacob Lerche <jacob.lerche@act-on.net>
2017-01-26 11:34:43 -0800

    Updates the front page test, for now

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Tee.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Go to game lobby"
   end
 end
